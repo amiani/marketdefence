b087b8b70f305432996267dbb60bdc3d7a228aa7
amiani <amianijohns@gmail.com>
2019-04-12 14:39:35 -0400

    added ContactListener skeleton

---
+++ Sources/ContactListener.hx
@@ -0,0 +1,12 @@
+import box2D.dynamics.B2ContactListener;
+import box2D.dynamics.contact.B2Contact;
+
+class ContactListener extends B2ContactListener {
+	public function beginContact(contact:B2Contact) {
+		
+	}
+
+	public function endContact(contact:B2Contact) {
+
+	}
+}
