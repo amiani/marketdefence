6421ef8032a2d173002aadde3a30f27ee96bfbbe
amiani <amianijohns@gmail.com>
2019-04-12 14:39:13 -0400

    added sensor to LaserTurret

--- Sources/LaserTurret.hx
+++ Sources/LaserTurret.hx
@@ -1,14 +1,33 @@
+import kha.Assets;
+import kha.math.FastVector2;
+import box2D.dynamics.B2World;
+import box2D.common.math.B2Vec2;
+import box2D.collision.shapes.B2PolygonShape;
+import box2D.dynamics.B2FixtureDef;
+
 class LaserTurret extends Body {
 	var gun : Sprite;
 	var base : Sprite;
+	var semiCircleShape : B2PolygonShape;
 
-	private function new(image:Image, parent:Node, world:B2World) {
+	public function new(position: FastVector2, parent:Node, world:B2World) {
 		super(position, parent, world, STATIC_BODY);
-		gun = new Sprite(Assets.images.turret1, 28, 54, 52, 3, this);
 		base = new Sprite(Assets.images.turret1, 64, 48, 35, 65, this);
+		gun = new Sprite(Assets.images.turret1, 28, 54, 52, 3, this);
+		gun.origin = new FastVector2(gun.width/2, gun.height);
+
+		var vertices = [for (k in 0...8) new B2Vec2(8*Math.cos(2*k*Math.PI/14), 8*Math.sin(2*k*Math.PI/14))];
+		semiCircleShape = B2PolygonShape.asVector(vertices, 8);
+		var radarFixtureDef = new B2FixtureDef();
+		radarFixtureDef.shape = semiCircleShape;
+		radarFixtureDef.density = 1;
+		radarFixtureDef.friction = .3;
+		radarFixtureDef.isSensor = true;
+		b2body.createFixture(radarFixtureDef);
 	}
 
 	override public function update(dt, ?parentWorldMatrix) {
+		
 		super.update(dt, parentWorldMatrix);
 	}
 }
