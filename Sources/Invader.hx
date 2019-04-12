b2552251d951c09c7d078129208f234a727a623d
amiani <amianijohns@gmail.com>
2019-04-12 18:43:46 -0400

    added fixture to Invader

--- Sources/Invader.hx
+++ Sources/Invader.hx
@@ -2,6 +2,8 @@ import kha.Assets;
 import kha.math.FastVector2;
 import box2D.dynamics.B2World;
 import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2FixtureDef;
+import box2D.collision.shapes.B2PolygonShape;
 
 class Invader extends Body {
 	var sprite : Sprite;
@@ -10,6 +12,11 @@ class Invader extends Body {
 		super(position, parent, world, DYNAMIC_BODY);
 		sprite = new Sprite(Assets.images.invadera, 64, 64, this);
 		angle = Math.PI;
+		var fixtureDef = new B2FixtureDef();
+		fixtureDef.shape = B2PolygonShape.asBox(.5, .5);
+		fixtureDef.friction = .3;
+		fixtureDef.userData = this;
+		b2body.createFixture(fixtureDef);
 	}
 
 	override private function updateBody(dt:Float) {
