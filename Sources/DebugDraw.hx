c731a4638cb84999662adbcc12c6687e7169c456
amiani <amianijohns@gmail.com>
2019-04-24 17:33:02 -0400

    fixed DebugDraw colors

--- Sources/DebugDraw.hx
+++ Sources/DebugDraw.hx
@@ -24,28 +24,28 @@ class DebugDraw extends B2DebugDraw {
 
 	override public function drawPolygon(vertices:Array<B2Vec2>, vertexCount:Int, color:B2Color) : Void{
 		var khaVertices = [for (v in vertices) new Vector2(v.x, v.y)];
-		//image.g2.color = color;
+		image.g2.color = 0xff000000 | color.color;
 		GraphicsExtension.drawPolygon(image.g2, 0, 0, khaVertices);
 	}
 
 	override public function drawSolidPolygon(vertices:Array <B2Vec2>, vertexCount:Int, color:B2Color) : Void{
 		var khaVertices = [for (v in vertices) new Vector2(v.x, v.y)];
-		image.g2.color = Color.Yellow;
+		image.g2.color = 0xff000000 | color.color;
 		GraphicsExtension.fillPolygon(image.g2, 0, 0, khaVertices);
 	}
 
 	override public function drawCircle(center:B2Vec2, radius:Float, color:B2Color) : Void{
-		image.g2.color = Color.Yellow;
+		image.g2.color = 0xff000000 | color.color;
 		GraphicsExtension.drawCircle(image.g2, center.x * m_drawScale, center.y * m_drawScale, radius * m_drawScale);
 	}
 	
 	override public function drawSolidCircle(center:B2Vec2, radius:Float, axis:B2Vec2, color:B2Color) : Void{
-		image.g2.color = Color.Yellow;
+		image.g2.color = 0xff000000 | color.color;
 		GraphicsExtension.fillCircle(image.g2, center.x * m_drawScale, center.y * m_drawScale, radius * m_drawScale);
 	}
 	
 	override public function drawSegment(p1:B2Vec2, p2:B2Vec2, color:B2Color) : Void{
-		image.g2.color = Color.Yellow;
+		image.g2.color = 0xff000000 | color.color;
 		image.g2.drawLine(p1.x, p1.y, p2.x, p2.y);
 	}
 
